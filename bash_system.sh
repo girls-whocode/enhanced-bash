00abaaee52a44b34812f0930522e5f65ae63066a
Jessica Brown <jessica@jbrowns.com>
2020-12-09 05:11:19 -0500

    Removed variables from controller Updated some notes Added the removed variables to the utility library

--- bash_system.sh
+++ bash_system.sh
@@ -12,58 +12,28 @@
 #	1. Currently it checks if the directory extists then files should be expected
 #	   this should change to check for files instead
 #	2. Check screen width for small screens (like cell phones) with terminal
-#	3. dirjump needs to become a module, not a library.
-#	4. Mixture of dirjump variables located in this file, they should be moved to
-#	   the dirjump module.
-#	5. Looking over this code, it looks as if the lib_utils library is loaded twice
-#	6. Clean up script for easier to find and read, this should be just a controller
-#	   to load each of the libraries, modules and overrides.
-#	7. Variables should be loaded from a library file, except path locations
-#	8. Create a config file for users to easily change custom variables
+#	DONE - 3. dirjump needs to become a module, not a library.
+#	DONE - 4. Mixture of dirjump variables located in this file, they should be moved to the dirjump module.
+#	DONE - 5. Looking over this code, it looks as if the lib_utils library is loaded twice
+#	DONE - 6. Clean up script for easier to find and read, this should be just a controller
+#	          to load each of the libraries, modules and overrides.
+#	DONE - 7. Variables should be loaded from a library file, except path locations
+#	DONE - 8. Create a config file for users to easily change custom variables
 
-scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-libLocation="${scriptPath}/lib"
-modLocation="${scriptPath}/modules"
-orLocation="${scriptPath}/overrides"
-thmLocation="${scriptPath}/themes"
-logsLocation="${scriptPath}/logs"
-userHomeLocation=$( getent passwd "$USER" | cut -d: -f6 )
-archiveLocation="${scriptPath}/.backup"
+scriptLocation="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+libLocation="${scriptLocation}/lib"
+modLocation="${scriptLocation}/modules"
+orLocation="${scriptLocation}/overrides"
+thmLocation="${scriptLocation}/themes"
+logsLocation="${scriptLocation}/logs"
+userHomeLocation=$( getent passwd "${USER}" | cut -d: -f6 )
+archiveLocation="${scriptLocation}/.backup"
 
-logFile="startup.log"
+if [ -f "${scriptLocation}/ebash.conf" ]; then
+	source "${scriptLocation}/ebash.conf"
+fi
 
-bashsystemVersion="4.0.1 l2m1"
-prompt_theme="pureblack"
-
-# TIMESTAMPS
-# ------------------------------------------------------
-# Prints the current date and time in a variety of formats:
-# ------------------------------------------------------
-now=$(LC_ALL=C date +"%m-%d-%Y %r")        				# Returns: 06-14-2015 10:34:40 PM
-logtime=$(LC_ALL=C date +"%Y-%m-%d %H:%M:%S")			# Returns: 2015-06-14 20:34:40
-datestamp=$(LC_ALL=C date +%Y-%m-%d)       				# Returns: 2015-06-14
-hourstamp=$(LC_ALL=C date +%r)             				# Returns: 10:34:40 PM
-timestamp=$(LC_ALL=C date +%Y%m%d_%H%M%S)  				# Returns: 20150614_223440
-today=$(LC_ALL=C date +"%m-%d-%Y")         				# Returns: 06-14-2015
-longdate=$(LC_ALL=C date +"%a, %d %b %Y %H:%M:%S %z")	# Returns: Sun, 10 Jan 2016 20:47:53 -0500
-gmtdate=$(LC_ALL=C date -u -R | sed 's/\+0000/GMT/')	# Returns: Wed, 13 Jan 2016 15:55:29 GMT
-
-# THISHOST
-# ------------------------------------------------------
-# Will print the current hostname of the computer the script
-# is being run on.
-# ------------------------------------------------------
-thisHost=$(hostname)
-
-bind -x '"\ec":"clear"' # Alt-C clears screen
-bind -x '"\er":"reload"' # Alt-R reloads environment
-bind -x '"\ev":"ver"' # Alt-V show Distro and version information
-bind -x '"\ew":"who"' # Alt-W show who information
-bind -x '"\eOP":"commands"' # F1 will display help system
-bind -x '"\eh":"commands"' # Alternitive (Alt-H) for help if F1 is not available
-bind -x '"\eOQ":"splashscreen"' # F2 will display the spashscreen
-bind -x '"\es":"splashscreen"' # Alternitive (Alt-S) for splashscreen if F2 is not available
-
+# Load the Lib Utils for logging functions, I am aware it is loaded again in the next block
 if [ -f "${libLocation}/lib_utils" ]; then
 	source "${libLocation}/lib_utils"
 else
@@ -125,18 +95,5 @@ if [ -d ${thmLocation} ]; then
 else
 	echo "[$(LC_ALL=C date +'%Y-%m-%d %H:%M:%S')]:[INFO]:[No overides to load]" >> ${logsLocation}/${logFile}
 fi
-
-# If you already use the number 0-9 as aliases,
-# feel free to comment out or delete these.
-# You can still use <dirjump_command> <dir path number>
-# to jump through the dir history
-# Source: https://unix.stackexchange.com/a/203160/307359
-i=0
-
-alias "$dirjump_command"="dirjump"
-while [ $i -le $history_size ]; do
-	alias $i="$dirjump_command $i"
-	i=`expr $i + 1`
-done
 
 __preload
